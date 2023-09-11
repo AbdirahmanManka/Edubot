63d5b437adbd07914582692faea473dff839d7c4
Silvanus Oketch <deseve48@gmail.com>
2023-09-11 06:21:21 +0300

    login

--- bot/urls.py
+++ bot/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),  # Login page
+    path('', views.login, name='login'),  # Login page
     path('logout/', views.user_logout, name='logout'),  # Logout view
     # path('', views.home, name='home'),  # Home page
     path('check_email/', views.check_email, name='check_email'),  # Check email in the database

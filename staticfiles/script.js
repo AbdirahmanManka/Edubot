d62767fad48a9b3fee552efae26d0fdd19dfde74
Silvanus Oketch <deseve48@gmail.com>
2023-09-11 09:04:38 +0300

    script-fixed

--- staticfiles/script.js
+++ staticfiles/script.js
@@ -14,49 +14,35 @@ function toggleChatContainer() {
 chatIcon.addEventListener('click', toggleChatContainer);
 closeBtn.addEventListener('click', toggleChatContainer);
 
+// Function to send a message to the chatbot and update the chat window
+function sendMessageToChatbot() {
+    const userMessage = userInput.value;
+    chatMessages.innerHTML += `<div class="user-message">${userMessage}</div>`;
+
+    // Clear the user input field
+    userInput.value = '';
+
+    // Send the user message to the server for processing
+    $.ajax({
+        url: '/chatbot_response/',  // Update with the correct URL
+        data: { message: userMessage },
+        dataType: 'json',
+        method: 'GET',
+        success: function (data) {
+            const chatbotResponse = data.response;
+            chatMessages.innerHTML += `<div class="chatbot-message">${chatbotResponse}</div>`;
+        },
+        error: function () {
+            // Handle errors if any
+            alert('An error occurred while processing your request.');
+        },
+    });
+}
 
 // Event listeners for user input
-sendButton.addEventListener('click', handleUserInput);
+sendButton.addEventListener('click', sendMessageToChatbot);
 userInput.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        handleUserInput();
-    }
-});
-
-$(document).ready(function () {
-    // Function to send a message to the chatbot and update the chat window
-    function sendMessageToChatbot() {
-        const userMessage = $('#user-input').val();
-        $('#chat-messages').append(`<div class="user-message">${userMessage}</div>`);
-
-    
-        $.ajax({
-            url: '/chatbot_response/',  // Update with the correct URL
-            data: { message: userMessage },
-            dataType: 'json',
-            method: 'GET',
-            success: function (data) {
-                const chatbotResponse = data.response;
-                $('#chat-messages').append(`<div class="chatbot-message">${chatbotResponse}</div>`);
-                // Clear the user input field
-                $('#user-input').val('');
-            },
-            error: function () {
-                // Handle errors if any
-                alert('An error occurred while processing your request.');
-            },
-        });
-    }
-
-    // Handle user input
-    $('#send-button').click(function () {
         sendMessageToChatbot();
-    });
-
-    // Handle Enter key press in the input field
-    $('#user-input').keypress(function (e) {
-        if (e.which === 13) {
-            sendMessageToChatbot();
-        }
-    });
+    }
 });

7596a0f01e2fbe1f6f0bc159e4670d36599f32bf
Silvanus Oketch <deseve48@gmail.com>
2023-09-15 00:16:31 +0300

    responses

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,5 @@
 import json
+import os
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from .models import Student
@@ -6,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
+from difflib import get_close_matches
 
 
 def login(request):
@@ -32,7 +34,20 @@ def login(request):
 
 def user_logout(request):
     logout(request)
-    return redirect('custom_login')  # Redirect to the custom_login page after logout
+    return redirect('login')  # Redirect to the custom_login page after logout
+
+def chatbot_response(request):
+    user_message = request.POST.get('userMessage')  
+    chatbot_response = generate_chatbot_response(user_message)
+    return JsonResponse({'response': chatbot_response})
+
+# Load the knowledge base from a JSON file
+def load_knowledge_base(file_name):
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')  # 'data' folder
+    file_path = os.path.join(data_dir, file_name)
+    with open(file_path, 'r') as file:
+        knowledge_base = json.load(file)
+    return knowledge_base
 
 def chatbot_response(request):
     user_message = request.POST.get('userMessage')  
@@ -40,11 +55,31 @@ def chatbot_response(request):
     return JsonResponse({'response': chatbot_response})
 
 def generate_chatbot_response(user_message):
-    user_message = user_message.lower()  # Convert the user message to lowercase for case-insensitive comparisons
+    user_message = user_message.lower()
 
-    if "1" in user_message:
-        return "You have selected Academics. How can I assist you with academics?"
-    elif "2" in user_message:
-        return "You have selected Non-Academics. What do you need help with regarding Non-Academics?"
+    # Load the knowledge base
+    knowledge_base = load_knowledge_base('knowledge_base.json')
+
+    # Extract the list of questions from the knowledge base
+    questions = [q["question"].lower() for q in knowledge_base["questions"]]
+
+    # Find the best match for the user's query
+    best_match = find_best_match(user_message, questions)
+
+    if best_match:
+        # If there's a match, retrieve the answer from the knowledge base
+        answer = get_answer_for_question(best_match, knowledge_base)
+        return answer
     else:
-        return "I'm sorry, I couldn't understand your request. Please choose either 1 or 2."
+        # If there's no match, provide a default response
+        return "I'm sorry, I couldn't understand your request."
+
+def find_best_match(user_question, questions):
+    # Use your existing get_close_matches function
+    matches = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return matches[0] if matches else None
+
+def get_answer_for_question(question, knowledge_base):
+    for q in knowledge_base["questions"]:
+        if q["question"].lower() == question:
+            return q["answer"]

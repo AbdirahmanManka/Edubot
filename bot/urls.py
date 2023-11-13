e1c9efc2a672def3f180b613f89acd0740d57a3f
Silvanus Oketch <97881965+Silvokyda@users.noreply.github.com>
2023-11-13 22:52:25 +0300

    adjust-urls

--- bot/urls.py
+++ bot/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
+    path('', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),  
     path('chatbot_response/', views.chatbot_response, name='chatbot_response'),
